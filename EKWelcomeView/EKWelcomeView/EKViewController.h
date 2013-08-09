ee7adadc801f17d28455daeda32c3ad9e94e2eb3
EvgenyKarkan <evgenykarkan@gmail.com>
2013-08-10 00:00:22 +0300

    Added project's template.

---
+++ EKWelcomeView/EKWelcomeView/EKViewController.h
@@ -0,0 +1,13 @@
+//
+//  EKViewController.h
+//  EKWelcomeView
+//
+//  Created by Каркан Евгений on 09.08.13.
+//  Copyright (c) 2013 EvgenyKarkan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EKViewController : UIViewController
+
+@end
---
+++ EKWelcomeView/EKWelcomeView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EKWelcomeView
+//
+//  Created by Каркан Евгений on 09.08.13.
+//  Copyright (c) 2013 EvgenyKarkan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EKAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([EKAppDelegate class]));
+	}
+}
